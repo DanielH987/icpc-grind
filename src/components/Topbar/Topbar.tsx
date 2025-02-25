83cdb95d1b4dc5627ecc72cb90164d45e03991cc
Daniel Hootini <hootinid@gmail.com>
2025-02-25 09:35:30 -1000

    Add TopBar component with a greeting message

---
+++ src/components/Topbar/Topbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type TopBarProps = {
+    
+};
+
+const TopBar:React.FC<TopBarProps> = () => {
+    
+    return <div>Have a good coding</div>
+}
+export default TopBar;
