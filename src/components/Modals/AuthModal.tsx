afc26d147dabbae63db719e13d9d02769c7bc651
Daniel Hootini <hootinid@gmail.com>
2025-02-24 16:09:20 -1000

    Update AuthModal close functionality to reset modal type to 'login'

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -44,7 +44,7 @@ export default AuthModal;
 function useCloseModal() {
 	const setAuthModalState = useSetRecoilState(authModalState);
 	const closeModal = () => {
-		setAuthModalState((prev) => ({ ...prev, isOpen: false }));
+		setAuthModalState((prev) => ({ ...prev, isOpen: false, type: 'login' }));
 	};
 
 	useEffect(() => {
