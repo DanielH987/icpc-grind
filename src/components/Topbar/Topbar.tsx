5d449aa4690893b2a56099b4fe857194a6877e7c
Daniel Hootini <hootinid@gmail.com>
2025-02-27 15:20:11 -1000

    Add Logout button and update authentication handling in Topbar component

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -2,11 +2,15 @@ import Link from "next/link";
 import React from "react";
 import { auth } from "@/firebase/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Logout from "@/components/Buttons/Lougout";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
 
 type TopbarProps = {};
 
 const Topbar: React.FC<TopbarProps> = () => {
-  const [user, setUser] = useAuthState(auth);
+  const [user] = useAuthState(auth);
+  const setAuthModalState = useSetRecoilState(authModalState);
 
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
@@ -27,7 +31,9 @@ const Topbar: React.FC<TopbarProps> = () => {
             </a>
           </div>
           {!user && (
-            <Link href="/auth">
+            <Link href="/auth" onClick={() => {
+              setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }));
+            }}>
               <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded">
                 Sign In
               </button>
@@ -45,6 +51,7 @@ const Topbar: React.FC<TopbarProps> = () => {
               </div>
             </div>
           )}
+          {user && <Logout />}
         </div>
       </div>
     </nav>
