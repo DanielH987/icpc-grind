1e75e74b33c141be0a35dfc0fca5ed78472a7593
Daniel Hootini <hootinid@gmail.com>
2025-03-04 09:37:40 -1000

    Add boilerplate code and EditorFooter to Playground component for improved user experience

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -4,21 +4,26 @@ import Split from 'react-split';
 import CodeMirror from '@uiw/react-codemirror';
 import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import { javascript } from '@codemirror/lang-javascript';
+import EditorFooter from './EditorFooter';
 
 type PlaygroundProps = {
 
 };
 
 const Playground: React.FC<PlaygroundProps> = () => {
+    const boilerPlate = `Function twoSum(nums: number[], target: number): number[] {
+    // Write your code here
+};
+    `;
 
     return (
-        <div className='flex flex-col bg-dark-layer-1 relative'>
+        <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
             <PreferenceNav />
 
             <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60, 40]} minSize={60}>
                 <div className="w-full overflow-auto">
                     <CodeMirror
-                        value='const a = 1;'
+                        value={boilerPlate}
                         theme={vscodeDark}
                         extensions={[javascript()]}
                         style={{ fontSize: 16 }}
@@ -26,12 +31,12 @@ const Playground: React.FC<PlaygroundProps> = () => {
                 </div>
                 <div className='w-full px-5 overflow-auto'>
                     {/* testcase heading */}
-					<div className='flex h-10 items-center space-x-6'>
-						<div className='relative flex h-full flex-col justify-center cursor-pointer'>
-							<div className='text-sm font-medium leading-5 text-white'>Testcases</div>
-							<hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white' />
-						</div>
-					</div>
+                    <div className='flex h-10 items-center space-x-6'>
+                        <div className='relative flex h-full flex-col justify-center cursor-pointer'>
+                            <div className='text-sm font-medium leading-5 text-white'>Testcases</div>
+                            <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white' />
+                        </div>
+                    </div>
 
                     <div className="flex">
                         {/* Testcase 1 */}
@@ -61,8 +66,22 @@ const Playground: React.FC<PlaygroundProps> = () => {
                             </div>
                         </div>
                     </div>
+
+                    <div className='font-semibold'>
+                        <p className='text-sm font-medium mt-4 text-white'>Input:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                            nums: [2, 7, 11, 15], target: 9
+                        </div>
+
+                        <p className='text-sm font-medium mt-4 text-white'>Output:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                            [0, 1]
+                        </div>
+                    </div>
                 </div>
             </Split>
+
+            <EditorFooter />
         </div>
     )
 }
