67fd0f0c38def5e656cdd71d70ebe15f1434d7ab
Daniel Hootini <hootinid@gmail.com>
2025-03-12 13:24:50 -1000

    Add Confetti effect to Playground component with responsive sizing

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Split from 'react-split';
 import ProblemDescription from './ProblemDescription/ProblemDescription';
 import Playground from './Playground/Playground';
+import Confetti from 'react-confetti';
+import useWindowSize from '@/hooks/useWindowSize';
 import { Problem } from '@/utils/types/problem';
 
 type WorkspaceProps = {
@@ -9,12 +11,19 @@ type WorkspaceProps = {
 };
 
 const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
+    const { width, height } = useWindowSize();
 
     return (
         <Split className='split' minSize={0}>
             <ProblemDescription problem={problem} />
             <div className='bg-dark-fill-2'>
                 <Playground problem={problem} />
+                <Confetti
+                    gravity={0.3}
+                    tweenDuration={4000}
+                    width={width}
+                    height={height}
+                />
             </div>
         </Split>
     )
