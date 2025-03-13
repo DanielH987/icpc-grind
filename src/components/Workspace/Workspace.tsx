050d90c7ed6f57a5ed93c75622b5a3b36533659e
Daniel Hootini <hootinid@gmail.com>
2025-03-12 14:38:46 -1000

    Add success state to Workspace component for conditional Confetti rendering

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Split from 'react-split';
 import ProblemDescription from './ProblemDescription/ProblemDescription';
 import Playground from './Playground/Playground';
@@ -12,18 +12,14 @@ type WorkspaceProps = {
 
 const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
     const { width, height } = useWindowSize();
+    const [success, setSuccess] = useState(false);
 
     return (
         <Split className='split' minSize={0}>
             <ProblemDescription problem={problem} />
             <div className='bg-dark-fill-2'>
-                <Playground problem={problem} />
-                <Confetti
-                    gravity={0.3}
-                    tweenDuration={4000}
-                    width={width}
-                    height={height}
-                />
+                <Playground problem={problem} setSuccess={setSuccess} />
+                {success && <Confetti gravity={0.3} tweenDuration={4000} width={width} height={height} />}
             </div>
         </Split>
     )
