0c8b2ba96ba2a9595751c07a9ec0106cb72a79b4
Daniel Hootini <hootinid@gmail.com>
2025-03-14 12:20:18 -1000

    Add transpilePackages configuration for CodeMirror dependencies

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  transpilePackages: [
+    "@codemirror/lang-javascript",
+    "@uiw/codemirror-theme-vscode",
+    "@uiw/react-codemirror",
+  ],
 };
 
 export default nextConfig;
