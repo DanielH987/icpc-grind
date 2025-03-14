afb117519c6ea520ca872f2eb9c4e925a1fc964a
Daniel Hootini <hootinid@gmail.com>
2025-03-13 16:11:33 -1000

    Add typewriter effect to Auth page for enhanced user engagement

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,12 +1,12 @@
 import Navbar from '@/components/Navbar/Navbar';
 import AuthModal from '@/components/Modals/AuthModal';
 import React, { useEffect, useState } from 'react';
-import Image from 'next/image';
 import { useRecoilValue } from 'recoil';
 import { authModalState } from '@/atoms/authModalAtom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/firebase';
 import { useRouter } from 'next/router';
+import { Typewriter } from 'react-simple-typewriter';
 
 type AuthPageProps = {
 
@@ -28,8 +28,19 @@ const AuthPage: React.FC<AuthPageProps> = () => {
     return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
         <div className='max-w-7xl mx-auto'>
             <Navbar />
-            <div className='flex justify-center items-center h-[calc(100vh-5rem)] pointer-events-none select-none'>
-                <Image src="/hero.png" alt="Hero Image" width={700} height={700} />
+            <div className="flex flex-col justify-center items-center h-[calc(100vh-25rem)] text-center">
+                <h1 className="text-4xl font-bold text-white">ICPC Grind</h1>
+                <p className="text-lg text-gray-300 mt-2">
+                    <Typewriter
+                        words={[
+                            "Sharpen your problem-solving skills.",
+                            "Ace your next ICPC contest.",
+                            "Practice. Compete. Succeed.",
+                            "Master algorithms and data structures."
+                        ]}
+                        loop
+                    />
+                </p>
             </div>
             {authModal.isOpen && <AuthModal />}
         </div>
