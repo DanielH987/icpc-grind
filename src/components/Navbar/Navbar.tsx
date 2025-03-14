8d537f602b26b603e0302f8cb9effe0ef42c2d02
Daniel Hootini <hootinid@gmail.com>
2025-03-13 16:11:36 -1000

    Update Navbar component to use favicon and improve branding display

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,21 +6,24 @@ import { useSetRecoilState } from 'recoil';
 
 type NavBarProps = {};
 
-const NavBar:React.FC<NavBarProps> = () => {
+const NavBar: React.FC<NavBarProps> = () => {
     const setAuthModalState = useSetRecoilState(authModalState);
     const handleClick = () => {
         setAuthModalState((prev) => ({ ...prev, isOpen: true }));
     };
-    
+
     return <div className='flex items-center justify-between sm:px-12 px-2  md:px-24'>
         <Link href="/" className='flex items-center justify-center h-20'>
-            <Image src='/logo.png' alt='logo' className='h-full' width={200} height={200}/>
+            <div className="flex items-center gap-2 text-xl text-white font-semibold cursor-pointer">
+                <Image src='/favicon.ico' alt='Logo' height={30} width={30} />
+                ICPC Grind
+            </div>
         </Link>
         <div className="flex items center">
             <button className='bg-brand-orange text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
             hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent
             transition duration-300 ease-in-out'
-            onClick={handleClick}
+                onClick={handleClick}
             >
                 Sign in
             </button>
