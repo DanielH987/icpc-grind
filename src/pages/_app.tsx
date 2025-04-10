8dfdfa346bbd0d5e0f0d8ffcea9c96d4c84461b4
Daniel Hootini <hootinid@gmail.com>
2025-04-10 09:54:00 -1000

    Add @vercel/analytics package and integrate Analytics component in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { RecoilRoot } from "recoil";
 import { ToastContainer } from "react-toastify";
+import { Analytics } from "@vercel/analytics/react";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
       <ToastContainer />
       <Component {...pageProps} />
+      <Analytics />
     </RecoilRoot>
   );
 }
