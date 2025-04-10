6b28d0f8f59f9ef60f2273eb9de6f9323657093a
Daniel Hootini <hootinid@gmail.com>
2025-04-10 09:20:25 -1000

    Add reset button to PreferenceNav for restoring starter code

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -166,7 +166,7 @@ const Playground: React.FC<PlaygroundProps> = ({ problem, setSuccess, setSolved 
 
     return (
         <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
-            <PreferenceNav settings={settings} setSettings={setSettings} />
+            <PreferenceNav settings={settings} setSettings={setSettings} setUserCode={setUserCode} problem={problem} />
 
             <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60, 40]} minSize={60}>
                 <div className="w-full overflow-auto">
