1777882c2782ae6365b85dde1f0eee12804f7520
Daniel Hootini <hootinid@gmail.com>
2025-04-10 14:04:42 -1000

    Fix language assignment in runSecret API to use original language from request

--- src/pages/api/runSecret.ts
+++ src/pages/api/runSecret.ts
@@ -33,7 +33,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       },
       body: JSON.stringify({
         code,
-        language: backendLanguage,
+        language: language,
         problemId,
       }),
     });
