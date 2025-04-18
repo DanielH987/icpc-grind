eb164ad4ce3f51ad7650571f5e54f9febd6cc436
Daniel Hootini <hootinid@gmail.com>
2025-04-18 13:42:48 -1000

    Add Java to languageMap in runSecret.ts

--- src/pages/api/runSecret.ts
+++ src/pages/api/runSecret.ts
@@ -17,6 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     javaScript: 'js',
     python: 'python',
     cpp: 'cpp',
+    java: 'java',
   };
 
   const backendLanguage = languageMap[language];
